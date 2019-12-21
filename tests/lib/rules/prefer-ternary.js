d4099a936eafaf73a6f26fc337b02d5ba1f89ce7
jmoore914 <jmoore914@gmail.com>
2019-12-21 16:13:59 -0500

    Almost everything working

--- tests/lib/rules/prefer-ternary.js
+++ tests/lib/rules/prefer-ternary.js
@@ -18,50 +18,218 @@ const RuleTester = require('eslint').RuleTester;
 //------------------------------------------------------------------------------
 
 var ruleTester = new RuleTester({parserOptions: {ecmaVersion: 6}});
+
 ruleTester.run('prefer-ternary', rule, {
 
 	valid: [
-		
 		{
-			code: `function foo(){
-                    if(a){
-                        b = 1;
-                    }
-                }`
+			code: `if(a){
+		                b = 1;
+		        }`
+		},
+		{
+			code: `if(a){
+		                b = 1
+		                doSomeStuff()
+		            }
+		            else{
+		                b = 2
+		            }`
+		},
+		{
+			code: `if(a){
+		                b = 1
+		            }
+		            else{
+		                c = 2
+		            }`
+		},
+		{
+			code: `if(a){
+		                b  = 1;
+		            } 
+		            else{
+		                b =  2;
+		            }`,
+			options: [{assignment: 'never'}]
+		},
+		{
+			code: `if(a){
+		                b  = 1;
+		            } 
+		            else{
+		                c =  2;
+		            }`,
+			options: [{assignment: 'same'}]
+		},
+		{
+			code: `if(a){
+		                b  = 1;
+		            } 
+		            else{
+		                c =  2;
+		            }`,
+			options: [{assignment: 'never'}]
 		},
 		{
 			code: `function foo(){
-                    if(a){
-                        b = 1
-                        doSomeStuff()
-                    }
+		                if(a){
+		                    return 1;
+		                } 
+		                else{
+		                    return 2;
+		                }
+		            }`,
+			options: [{return: false}]
+		},
+		{
+			code: `if(a){
+                        foo();
+                    } 
                     else{
-                        b = 2
-                    }
-            }`
+                        bar();
+                    }`
+		},
+		{
+			code: `if(a){
+                        foo();
+                    } 
+                    else{
+                        bar();
+                    }`,
+			options: [{call: false}]
 		}
 		
 	],
 
 	invalid: [
 		{
-			code: `
-                function foo(){
-                    if(a){
-                        b  = 1;
-                    } 
-                    else{
-                        b =  2;
-                    }
-                }
-            `,
-			output: `
-                function foo(){
-                    b = a ? 1 : 2
-                }
-            `,
+			code: `if(a){
+		        b  = 1;
+		    } 
+		    else{
+		        b =  2;
+		    }`,
+			output: 'b = a ? 1 : 2',
 			errors: [
-				{column: 21, line: 3, type: 'IfStatement', messageId: 'preferTernary'}
+				{column: 1, line: 1, type: 'IfStatement', messageId: 'preferTernary'}
+			]
+		},
+		{
+			code: `if(a){
+		                b  = 1;
+		            } 
+		            else{
+		                b =  2;
+		            }`,
+			options: ['always'],
+			output: 'b = a ? 1 : 2',
+			errors: [
+				{column: 1, line: 1, type: 'IfStatement', messageId: 'preferTernary'}
+			]
+		},
+		{
+			code: `if(a){
+		                b  = 1;
+		            } 
+		            else{
+		                b =  2;
+		            }`,
+			options: [{assignment: 'same'}],
+			output: 'b = a ? 1 : 2',
+			errors: [
+				{column: 1, line: 1, type: 'IfStatement', messageId: 'preferTernary'}
+			]
+		},
+		{
+			code: `if(a){
+		                b  = 1;
+		            } 
+		            else{
+		                b =  2;
+		            }`,
+			options: [{assignment: 'any'}],
+			output: 'b = a ? 1 : 2',
+			errors: [
+				{column: 1, line: 1, type: 'IfStatement', messageId: 'preferTernary'}
+			]
+		},
+        
+		{
+			code: `if(a){
+		                b = 1;
+		            } 
+		            else{
+		                c = 2;
+		            }`,
+			options: ['always'],
+			output: 'a ? b=1 : c=2',
+			errors: [
+				{column: 1, line: 1, type: 'IfStatement', messageId: 'preferTernary'}
+			]
+		},
+		{
+			code: `if(a){
+		                b  = 1;
+		            } 
+		            else{
+		                c =  2;
+		            }`,
+			options: [{assignment: 'any'}],
+			output: 'a ? b=1 : c=2',
+			errors: [
+				{column: 1, line: 1, type: 'IfStatement', messageId: 'preferTernary'}
+			]
+		},
+               
+		{
+			code: 'function foo(){if(a){return 1;}else{return 2;}}',
+			output: 'function foo(){return a ? 1 : 2}',
+			errors: [
+				{column: 16, line: 1, type: 'IfStatement', messageId: 'preferTernary'}
+			]
+		},
+		{
+			code: 'function foo(){if(a){return 1;}else{return 2;}}',
+			options: ['always'],
+			output: 'function foo(){return a ? 1 : 2}',
+			errors: [
+				{column: 16, line: 1, type: 'IfStatement', messageId: 'preferTernary'}
+			]
+		},
+		{
+			code: 'function foo(){if(a){return 1;}else{return 2;}}',
+			options: [{return: true}],
+			output: 'function foo(){return a ? 1 : 2}',
+			errors: [
+				{column: 16, line: 1, type: 'IfStatement', messageId: 'preferTernary'}
+			]
+		},
+        
+		{
+			code: `if(a){
+		                foo();
+		            } 
+		            else{
+		                bar();
+		            }`,
+			options: ['always'],
+			output: 'a ? foo() : bar()',
+			errors: [
+				{column: 1, line: 1, type: 'IfStatement', messageId: 'preferTernary'}
+			]
+		},
+		{
+			code: `if(a){
+		                foo();
+		            } 
+		            else{
+		                bar();
+		            }`,
+			options: [{call: true}],
+			output: 'a ? foo() : bar()',
+			errors: [
+				{column: 1, line: 1, type: 'IfStatement', messageId: 'preferTernary'}
 			]
 		}
 	]
